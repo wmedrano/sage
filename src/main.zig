56c57241d3fe1db6883c259c4829ae92ef63fbc5
Will Medrano <will.s.medrano@gmail.com>
2024-07-19 16:16:26 -0700

    Print out the main script.

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,66 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    const stdout = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout);
+    defer bw.flush() catch {};
+    const script = try Script.init("main.sage", std.heap.page_allocator);
+    try script.print(bw.writer());
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+const Script = struct {
+    filepath: []u8,
+    contents: []u8,
+    allocator: std.mem.Allocator,
+
+    pub fn init(filepath: []const u8, allocator: std.mem.Allocator) !Script {
+        const input_file = try std.fs.cwd().openFile(filepath, .{});
+        const file_size = try input_file.getEndPos();
+        const contents = try allocator.alloc(u8, file_size);
+        const read_size = try input_file.readAll(contents);
+        if (read_size != file_size) {
+            return error.UnexpectedNumberOfBytesRead;
+        }
+
+        const filepath_copy = try allocator.alloc(u8, filepath.len);
+        std.mem.copyForwards(u8, filepath_copy, filepath);
+
+        return .{
+            .filepath = filepath_copy,
+            .contents = contents,
+            .allocator = allocator,
+        };
+    }
+
+    pub fn free(self: *Script) void {
+        self.allocator.free(self.filepath);
+        self.allocator.free(self.contents);
+    }
+
+    pub fn print(self: *const Script, writer: anytype) !void {
+        _ = try writer.print("Script: {s}\n", .{self.filepath});
+        _ = try writer.print("{s}", .{self.contents});
+    }
+};
+
+fn print_script_contents(script_filepath: []const u8, writer: anytype, comptime allocator: std.mem.Allocator) !void {
+    var script = try Script.init(script_filepath, allocator);
+    defer script.free();
+    try script.print(writer);
+}
+
+test "print_script_contents reads file" {
+    var result = std.ArrayList(u8).init(std.testing.allocator);
+    defer result.deinit();
+    try print_script_contents("main.sage", result.writer(), std.testing.allocator);
+    try std.testing.expectEqualStrings("Script: main.sage\n(+ 1 2 3 4)", result.items);
+}
+
+test "print_script_contents with non-existant file returns an error" {
+    const writer = struct {
+        pub fn print(_: anytype, _: anytype) !usize {
+            unreachable;
+        }
+    };
+    try std.testing.expectError(error.FileNotFound, print_script_contents("does-not-exist", writer, std.testing.allocator));
 }
